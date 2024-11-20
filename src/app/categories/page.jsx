75ccbb267c54b0f8bb8105ff256b086f2a48cf7b
KhaledShrief <139564204+KhaledShrief@users.noreply.github.com>
2024-11-20 23:10:13 +0200

    cart response

--- src/app/categories/page.jsx
+++ src/app/categories/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button, Input } from "@nextui-org/react";
 import Link from "next/link";
-import { useRouter, usePathname } from "next/navigation";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
 import { GiEmptyWoodBucketHandle } from "react-icons/gi";
 import { useToast } from "../contexts/CustomToast";
@@ -12,6 +12,7 @@ import { AiOutlineShopping } from "react-icons/ai";
 const Categories = () => {
     const router = useRouter();
     const pathname = usePathname();
+    const searchParams = useSearchParams();
     const [category, setCategory] = useState("");
     const [categories, setCategories] = useState([]);
     const { showToast } = useToast();
@@ -96,6 +97,11 @@ const Categories = () => {
             showToast(`${product.name} added to your cart!`);
         }
     };
+
+    useEffect(() => {
+        const initialCategory = searchParams.get("category") || "";
+        setCategory(initialCategory);
+    }, [searchParams]);
 
     useEffect(() => {
         fetchCategories();
